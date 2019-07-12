7650914c485d9f64577578cff364bc27acb9e7cd
Mano Sriram <mano.sriram0@gmail.com>
2019-07-12 14:51:37 +0530

    First Commit. Add BoilerPlate

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, CircularProgress } from "@material-ui/core";
+
+const App = () => {
+  return (
+    <>
+      <Container maxWidth="md">
+        <h3>App.JS</h3>
+        {/* <CircularProgress size={140} /> */}
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, facilis
+          exercitationem vel totam nulla in voluptatem et molestias! Ab, quae
+          est soluta aspernatur earum dolores provident quas consequatur
+          molestiae assumenda! A repellat dolor doloremque nisi amet eum maiores
+          odit mollitia ullam totam odio quos alias hic dicta, asperiores
+          voluptatem et? Commodi dolorem perferendis, consequuntur nisi eos
+          placeat eveniet quaerat nesciunt?
+        </p>
+      </Container>
+    </>
+  );
+};
+
+export default App;
